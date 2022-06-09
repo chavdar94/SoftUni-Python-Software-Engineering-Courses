b8ca17e147792729013a58769e4bffbeed15a34e
chavdar94 <106109451+chavdar94@users.noreply.github.com>
2022-06-09 19:22:42 +0300

    Updater factorial_divison with different solutions

--- Python - Fundamentals/functions/exercise/fctorial_division.py
+++ Python - Fundamentals/functions/exercise/fctorial_division.py
@@ -1,24 +1,28 @@
-# from math import factorial
+from math import factorial
 
 
 def factorial_func(n1, n2):
+    #  Using math library and factorial function with storing the sums in variables
     # if n1 >= 0 and n2 >= 0:
     #     first_fact = factorial(n1)
     #     second_fact = factorial(n2)
     #     return f"{first_fact / second_fact:.2f}"
-    # else:
-    #     return "Cant factorial negative number!"
-    if n1 >= 0:
-        first_sum = 1
-        for i in range(1, n1 + 1):
-            first_sum *= i
 
-    if n2 >= 0:
-        second_sum = 1
-        for i in range(1, n2 + 1):
-            second_sum *= i
-    result = first_sum / second_sum
-    return f"{result:.2f}"
+    #  Using no libraries and also storing the sums in variables ,so we can access each sum separately
+    # if n1 >= 0:
+    #     first_sum = 1
+    #     for i in range(1, n1 + 1):
+    #         first_sum *= i
+    #
+    # if n2 >= 0:
+    #     second_sum = 1
+    #     for i in range(1, n2 + 1):
+    #         second_sum *= i
+    # result = first_sum / second_sum
+    # return f"{result:.2f}"
+
+    #  Shortest way to find the final result also using math library
+    return f"{(factorial(n1) / factorial(n2)):.2f}"
 
 
 first_number, second_number = int(input()), int(input())
