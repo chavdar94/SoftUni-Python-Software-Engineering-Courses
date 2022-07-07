fa64ac52e8376ce30670c23516c7ae3afe52b2c5
chavdar tonchev <106109451+chavdar94@users.noreply.github.com>
2022-07-07 20:23:48 +0300

    added new task

---
+++ Python - Fundamentals/dictioanires/exercise/04.phonebook.py
@@ -0,0 +1,22 @@
+information = input()
+phonebook = {}
+
+
+while '-' in information:
+    person_info = information.split('-')
+    name = person_info[0]
+    number = person_info[1]
+    if name not in phonebook:
+        phonebook[name] = number
+    else:
+        phonebook[name] = number
+
+    information = input()
+
+
+for person in range(int(information)):
+    current_person = input()
+    if current_person in phonebook:
+        print(f'{current_person} -> {phonebook[current_person]}')
+    else:
+        print(f'Contact {current_person} does not exist.')
