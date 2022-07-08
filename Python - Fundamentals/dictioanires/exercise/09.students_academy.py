979277afc4a2af7dd26f2543b101480be4322a6e
chavdar94 <106109451+chavdar94@users.noreply.github.com>
2022-07-08 12:50:59 +0300

    added new task

--- Python - Fundamentals/dictioanires/exercise/09.students_academy.py
+++ Python - Fundamentals/dictioanires/exercise/09.students_academy.py
@@ -10,7 +10,16 @@ for i in range(number_of_students):
         students[student_name] = []
     students[student_name].append(grade)
 
+#  dictionary comprehension
+# passed_students = {key: value for (key,value) in students.items() if sum(value)/len(value) >= 4.5}
+#
+# for key, value in passed_students.items():
+# #     average = sum(value) / len(value)
+# #     print(f"{key} -> {average:.2f}")
+
+#  normal for loop
 passed_students = {}
+
 for key in students:
     average = sum(students[key]) / len(students[key])
     if average >= 4.5:
