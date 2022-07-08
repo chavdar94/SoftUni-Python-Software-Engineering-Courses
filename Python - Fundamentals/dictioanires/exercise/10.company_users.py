a5cb8cd5f5db957d5a595d3d5a5ba99fdb7786b0
chavdar94 <106109451+chavdar94@users.noreply.github.com>
2022-07-08 16:15:26 +0300

    added new task

---
+++ Python - Fundamentals/dictioanires/exercise/10.company_users.py
@@ -0,0 +1,22 @@
+companies = {}
+
+command = input()
+while command != 'End':
+
+    information = command.split(' -> ')
+    company_name = information[0]
+    employee_id = information[1]
+    if company_name not in companies:
+        companies[company_name] = []
+        if employee_id not in companies[company_name]:
+            companies[company_name].append(employee_id)
+    else:
+        if employee_id not in companies[company_name]:
+            companies[company_name].append(employee_id)
+
+    command = input()
+
+for key, value in companies.items():
+    print(key)
+    for id in value:
+        print(f"-- {id}")
