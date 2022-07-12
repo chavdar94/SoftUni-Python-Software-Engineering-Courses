ee3c396a0ad1903a4b2252ea560053b68064639d
chavdar94 <106109451+chavdar94@users.noreply.github.com>
2022-07-12 11:33:47 +0300

    added new task

--- Python - Fundamentals/dictioanires/more_exercises/02.judge.py
+++ Python - Fundamentals/dictioanires/more_exercises/02.judge.py
@@ -15,11 +15,7 @@ while data != 'no more time':
     if judge[contest][username] < points:
         users[username] += points - judge[contest][username]
         judge[contest][username] = points
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> origin/main
     data = input()
 
 for current_contest in judge.keys():
@@ -29,8 +25,4 @@ for current_contest in judge.keys():
 
 print('Individual standings:')
 for pos, (user, points,) in enumerate(sorted(users.items(), key=lambda x: (-x[1], x[0])), 1):
-<<<<<<< HEAD
     print(f'{pos}. {user} -> {points}')
-=======
-    print(f'{pos}. {user} -> {points}')
->>>>>>> origin/main
