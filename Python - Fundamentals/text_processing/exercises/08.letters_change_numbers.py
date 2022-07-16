e2739920f9fa8face3b1f0e73cedcdb2876328f4
chavdar tonchev <chokobg@gmail.com>
2022-07-16 16:24:16 +0300

    added new tasks

---
+++ Python - Fundamentals/text_processing/exercises/08.letters_change_numbers.py
@@ -0,0 +1,31 @@
+data = input().split()
+
+total_sum = 0
+for text in data:
+    number = ''
+    letter = ''
+    current_sum = 0
+    for symbol in text:
+        if symbol.isdigit():
+            number += symbol
+    current_sum = int(number)
+    if text[0].isupper():
+        letter_position = ord(text[0]) - 64
+        current_sum = current_sum / letter_position
+
+    elif text[0].islower():
+        letter_position = ord(text[0]) - 96
+        current_sum = current_sum * letter_position
+
+    if text[-1].isupper():
+        letter_position = ord(text[-1]) - 64
+        current_sum = current_sum - letter_position
+
+    elif text[-1].islower():
+        letter_position = ord(text[-1]) - 96
+        current_sum = current_sum + letter_position
+
+    total_sum += current_sum
+    current_sum = 0
+
+print(f"{total_sum:.2f}")
