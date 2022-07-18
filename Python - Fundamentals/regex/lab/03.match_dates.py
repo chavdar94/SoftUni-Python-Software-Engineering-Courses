807b752acc249d37348a5c0d04e057dd25502802
chavdar94 <106109451+chavdar94@users.noreply.github.com>
2022-07-18 15:09:41 +0300

    added new task

---
+++ Python - Fundamentals/regex/lab/03.match_dates.py
@@ -0,0 +1,8 @@
+import re
+
+pattern = r'\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b'
+dates = input()
+matches = re.finditer(pattern, dates)
+
+for match in matches:
+    print(f'Day: {match[0]}, Month: {match[2]}, Year: {match[3]}')
